51c55103abb387446efa735e8c8584d626f8e426
ireneuszcierpisz <>
2023-08-28 16:48:04 +0000

    Add aria-label descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,7 +99,7 @@ function collectData() {
         for (let i = 0; i < npvArray.length; i++) {
             npv += npvArray[i];
         }
-        console.log(npv);
+        console.log('npv: ' + npv);
 
         if (npv > 0) {
             document.getElementById("message").innerHTML = `<b>You sent data! See your NPV</b>: <h3 style="font-family:sans-serif; color:blue; border:1px solid grey; background-color:whitesmoke; width: auto; height:auto; text-align:center; padding: 4px 1.5rem; display:inline-block;">+${npv}</h3><p style="display:inline-block; margin-left:10px;"><span style="padding:0 5px;"></span><b>Your intuition is great! You are the winner!</b></p>`;
